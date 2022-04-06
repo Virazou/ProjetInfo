12685d392daf02aec762ce3fa4b429a0dddaf6db
Matteo <mascaro.matteo@outlook.com>
2022-04-06 13:41:33 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+class Circuit(list):
+
+
+    def __init__(self, nom,m):
+        self.__nom = nom
+        self.__m = m
+
+
+
+class Vehicule:
+    def __init__(self,nom,adherence,puissance,carburant,posx,posy):
+        self.__nom = nom
+        self.__adherence = adherence
+        self.__carburant = carburant
+        self.__posx= posx
+        self.__posy = posy
+
+
+    #def controler(self):
+
+
+
+
+
+
+
+
+
